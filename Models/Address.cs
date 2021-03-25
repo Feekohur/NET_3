cfb215f857cef75247371bb85167cd6fb5e0548f
Michał Król <77741@student.pb.edu.pl>
2021-03-25 11:34:30 +0100

    Dodaj pliki projektów.

---
+++ Models/Address.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace NET_3.Models
+{
+    public class Address
+    {
+        /*[Display(Name = "Twoja ulubiona ulica")]
+        [Required(ErrorMessage = "Podaj ulice")]
+        public string Street { get; set; }
+
+        [Display(Name = "Zipcode")]
+        [StringLength(60, MinimumLength = 3), Required]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "City")]
+        [Required(ErrorMessage = "Podaj miasto")]
+        public string City { get; set; }*/
+
+        [Display(Name = "Number")]
+        [Required]
+        [Range(1, 1000)]
+        public int Number { get; set; }
+
+public string Result { get; set; }
+
+        public DateTime Date { get; set; }
+
+
+    }
+}
