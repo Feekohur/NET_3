22f0efae84cb6f2f9d3fa846fd8fe78cfbbab960
Michał Król <77741@student.pb.edu.pl>
2021-03-25 11:34:30 +0100

    Dodaj pliki projektów.

---
+++ Models/Address.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace NET_3.Models
+{
+    public class Address
+    {
+        /*[Display(Name = "Twoja ulubiona ulica")]
+        [Required(ErrorMessage = "Podaj ulice")]
+        public string Street { get; set; }
+
+        [Display(Name = "Zipcode")]
+        [StringLength(60, MinimumLength = 3), Required]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "City")]
+        [Required(ErrorMessage = "Podaj miasto")]
+        public string City { get; set; }*/
+
+        [Display(Name = "Number")]
+        [Required]
+        [Range(1, 1000)]
+        public int Number { get; set; }
+
+public string Result { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public Address()
+        {
+
+        }
+
+        public void MakesValues(int number)
+        {
+            if(number % 3 == 0 && number % 5 != 0)
+            {
+                Result = "Fizz";
+                Date = DateTime.Now;
+            }
+
+            else if (number % 3 != 0 && number % 5 == 0)
+            {
+                Result = "Buzz";
+                Date = DateTime.Now;
+            }
+
+            else if (number % 3 == 0 && number % 5 == 0)
+            {
+                Result = "FizzBuzz";
+                Date = DateTime.Now;
+            }
+
+            else 
+            {
+                Result = "Brak";
+                Date = DateTime.Now;
+            }
+
+        }
+
+
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using NET_3.Models;
+
+namespace NET_3.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public Address Address { get; set; }
+
+
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                Address.MakesValues(Address.Number);
+                HttpContext.Session.SetString("Ostatnio szukane", JsonConvert.SerializeObject(Address));
+                //return RedirectToPage("./Address");
+                return Page();
+            }
+            return Page();
+        }
+
+
+    }
+}
